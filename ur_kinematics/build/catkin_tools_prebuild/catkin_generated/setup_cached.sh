dac0a518b3a21caa59b1b52e37780380f456e7aa
Prof. Lee <leegs@case.edu>
2020-10-21 09:39:23 -0400

    Including Universal Robotics IK package internally and finished sample files
    Logical cameras were added to all the bins and agvs, also moved around the parts in the bins.  Brought the Universal Robotics IK package into this package for simplicity.

---
+++ ur_kinematics/build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/leegs/universal_robot/ur_kinematics/devel/.private/catkin_tools_prebuild:$CMAKE_PREFIX_PATH"
+export GAZEBO_MODEL_PATH="/home/leegs/ariac_ws/src/ARIAC/nist_gear/models:$GAZEBO_MODEL_PATH"
+export PWD="/home/leegs/universal_robot/ur_kinematics/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/leegs/universal_robot/ur_kinematics/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/leegs/universal_robot/ur_kinematics/build/catkin_tools_prebuild:/home/leegs/ariac_ws/src/ARIAC/ariac:/home/leegs/ariac_ws/src/ARIAC/gantry_moveit_config:/home/leegs/ariac_ws/src/ARIAC/nist_gear:/home/leegs/ariac_ws/src/ARIAC/ariac_example:/home/leegs/ariac_ws/src/cwru_ariac_2020:/home/leegs/ariac_ws/src/ARIAC/test_ariac:/opt/ros/melodic/share"
---
+++ ur_kinematics/devel/local_setup.zsh
@@ -0,0 +1,1 @@
+/home/leegs/universal_robot/ur_kinematics/devel/.private/catkin_tools_prebuild/local_setup.zsh
