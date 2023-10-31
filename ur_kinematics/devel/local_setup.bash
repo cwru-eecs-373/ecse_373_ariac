d234b3fddd5b50cfe475365bb0748df9b6b1c382
Prof. Lee <leegs@case.edu>
2023-10-31 16:39:12 -0400

    Clean up and build updating
    Updated the minimum cmake version to stop warnings.
    
    Removed unused folders.

--- ur_kinematics/devel/local_setup.bash
+++
@@ -1,1 +0,0 @@
-/home/leegs/universal_robot/ur_kinematics/devel/.private/catkin_tools_prebuild/local_setup.bash
--- ur_kinematics/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/leegs/universal_robot/ur_kinematics/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
